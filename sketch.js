67c3cc73c9f7e4b81163a053428d250c693f296d
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-05-15 03:50:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,14 @@
 var canvas,bg;
-var together;
-var tom, tomImg1,tomImg2;
-var jerry, jerryImg1,jerryImg2;
+
+
+//create variables
+
 
 function preload() {
-    bg = loadImage("images/garden.png");
-    tomImg1= loadAnimation("images/tomOne.png");
-    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
-    tomImg3= loadAnimation("images/tomFour.png");
-    jerryImg1=loadAnimation("images/jerryOne.png");
-    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-    jerryImg3=loadAnimation("images/jerryFour.png");
+   //load images
+    
+    
+    
 
 }
 
@@ -18,18 +16,18 @@ function setup(){
     canvas = createCanvas(1000,800);
 
     tom = createSprite(870, 600);
-    tom.addAnimation("tomSleeping", tomImg1);
+   //add animation
     tom.scale = 0.2;
 
     jerry = createSprite(200, 600);
-    jerry.addAnimation("jerryStanding", jerryImg1);
+    //add animation
     jerry.scale = 0.15;
 
 }
 
 function draw() {
 
-    background(bg);
+    background();
 
     if(tom.x - jerry.x < (tom.width - jerry.width)/2)
     { 
