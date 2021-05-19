67fb4217d8fc3b376b0fc1668913647f10ec505c
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-05-20 03:34:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,29 @@
 var canvas,bg;
-
-
-//create variables
-
+var together;
+var tom, tomImg1,tomImg2;
+var jerry, jerryImg1,jerryImg2;
 
 function preload() {
-   //load images
-    
-    
-    
+    bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/tomOne.png");
+    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
+    tomImg3= loadAnimation("images/tomFour.png");
+    jerryImg1=loadAnimation("images/jerryOne.png");
+    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
+    jerryImg3=loadAnimation("images/jerryFour.png");
 
 }
+
 
 function setup(){
     canvas = createCanvas(1000,800);
 
     tom = createSprite(870, 600);
-   //add animation
+    tom.addAnimation("tomSleeping", tomImg1);
     tom.scale = 0.2;
 
     jerry = createSprite(200, 600);
-    //add animation
+    jerry.addAnimation("jerryStanding", jerryImg1);
     jerry.scale = 0.15;
 
 }
